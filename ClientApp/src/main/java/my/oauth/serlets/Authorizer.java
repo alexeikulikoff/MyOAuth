f144b3300d8eb028f21bd0e5abe461a3aafbdc4f
alexeikulikoff <alexei.kulikoff@gmail.com>
2020-03-28 12:23:52 +0300

    add github suppert

--- ClientApp/src/main/java/my/oauth/serlets/Authorizer.java
+++ ClientApp/src/main/java/my/oauth/serlets/Authorizer.java
@@ -20,21 +20,24 @@ public class Authorizer extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
 		String response_type = "code";
-		String[] scopes = Client.getScope();
-		String client_id = Client.getClient_id();
+		//String[] scopes = Client.getScope();
+		String scopes = "read:user";
+		//String client_id = Client.getClient_id();
+		String client_id = "611f43c030371f5d7297";
+	//	String redirect_uri = Client.getRedirect_uris()[0];
 		String redirect_uri = Client.getRedirect_uris()[0];
 		String state = UUID.randomUUID().toString().replace("-", "");
 		
 		
-		String auth_url = "http://localhost:8002/authorize";
+		//String auth_url = "http://localhost:8002/authorize";
+		String auth_url = "https://github.com/login/oauth/authorize/";
 		
-
-		String scope = "";
-		for (String s : scopes) {
-			scope += " " +  s;
-		}
+		String scope = "read:user";
+		/////for (String s : scopes) {
+		//	scope += " " +  s;
+		//}
 		
-		scope = scope.trim();
+		//scope = scope.trim();
 		
 		
 		String url_params = "response_type=" + response_type + "&scope=" + scope + "&client_id=" + client_id
--- ClientApp/src/main/java/my/oauth/serlets/CallBack.java
+++ ClientApp/src/main/java/my/oauth/serlets/CallBack.java
@@ -15,10 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -29,65 +31,138 @@ import com.google.gson.Gson;
 
 import my.oauth.app.Token;
 import my.oauth.utils.Client;
+import my.oauth.utils.GitHubToken;
 import my.oauth.utils.TokenResponce;
 
 public class CallBack extends HttpServlet implements TheamlefServlet {
-	
+
 	private Token token;
-	
-	public CallBack( Token token ) {
+
+	public CallBack(Token token) {
 		super();
 		this.token = token;
 	}
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		
+
 		String code = req.getParameter("code");
-		
+		String state = req.getParameter("state");
+
+		System.out.println(code);
+		System.out.println(state);
 		HttpClient httpclient = HttpClients.createDefault();
-		HttpPost httppost = new HttpPost("http://localhost:8002/token");
+
+		HttpPost httppost = new HttpPost("https://github.com/login/oauth/access_token");
 		List<NameValuePair> params = new ArrayList<>();
-		params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+		params.add(new BasicNameValuePair("client_id", "611f43c030371f5d7297"));
+		params.add(new BasicNameValuePair("client_secret", "0d013731236e856bf283698a4cfd1d7ed2ed9439"));
 		params.add(new BasicNameValuePair("code", code));
 		params.add(new BasicNameValuePair("redirect_uri", Client.getRedirect_uris()[0]));
+		params.add(new BasicNameValuePair("state", state));
 
 		httppost.setEntity(new UrlEncodedFormEntity(params));
 		httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		httppost.setHeader("Authorization", "Basic " + Client.getClient_id() + ":" + Base64.getEncoder().encodeToString(Client.getClient_secret().getBytes()));
+		httppost.setHeader("Accept", "application/json");
 		
+	//	httppost.setHeader("Authorization", "Basic " + Client.getClient_id() + ":"
+	//			+ Base64.getEncoder().encodeToString(Client.getClient_secret().getBytes()));
+
 		TokenResponce tokenResponce = null;
 		StringBuilder sb = new StringBuilder();
-		try( CloseableHttpResponse response = (CloseableHttpResponse) httpclient.execute(httppost) ) {
-			    HttpEntity entity = response.getEntity();
-			    if (entity != null) {
-			      try ( InputStream instream = entity.getContent(); 
-			    		BufferedReader reader = new BufferedReader( new InputStreamReader(instream) );){
-			        	String s;
-			        	while((s = reader.readLine())!= null) {
-			        		sb.append(s);
-			        	}
-			        }
-			    }
-		} 
+		try (CloseableHttpResponse response = (CloseableHttpResponse) httpclient.execute(httppost)) {
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				try (InputStream instream = entity.getContent();
+						BufferedReader reader = new BufferedReader(new InputStreamReader(instream));) {
+					String s;
+					while ((s = reader.readLine()) != null) {
+						System.out.println(s);
+						sb.append(s);
+					}
+				}
+			}
+		}
 		
-		tokenResponce = new Gson().fromJson( sb.toString() , TokenResponce.class);
+		GitHubToken gitToken = new Gson().fromJson( sb.toString() , GitHubToken.class);
 		
-		token.setAccess_token(tokenResponce.getAccess_token());
-		token.setScope(tokenResponce.getScope());
-		token.setToken_type(tokenResponce.getToken_type());
+		System.out.println( gitToken);
+		
+		HttpClient httpclient2 = HttpClients.createDefault();
+
+		HttpGet httppost2 = new HttpGet("https://api.github.com/user");
+		List<NameValuePair> params2 = new ArrayList<>();
+		//params2.add(new BasicNameValuePair("client_id", "611f43c030371f5d7297"));
+	//	params2.add(new BasicNameValuePair("client_secret", "4df850852ddd9cffde94b8dc9556f89ca95c5652"));
+	//	params2.add(new BasicNameValuePair("code", code));
+	//	params2.add(new BasicNameValuePair("redirect_uri", Client.getRedirect_uris()[0]));
+		//params2.add(new BasicNameValuePair("Authorization", gitToken.getAccess_token() + " OAUTH-TOKEN"));
+
+	
+		httppost2.setHeader("Authorization", "token " +  gitToken.getAccess_token());
 		
 		
+	//	httppost.setHeader("Authorization", "Basic " + Client.getClient_id() + ":"
+	//			+ Base64.getEncoder().encodeToString(Client.getClient_secret().getBytes()));
+
+	
+		StringBuilder sb2 = new StringBuilder();
+		try (CloseableHttpResponse response2 = (CloseableHttpResponse) httpclient2.execute(httppost2)) {
+			HttpEntity entity2 = response2.getEntity();
+			if (entity2 != null) {
+				try (InputStream instream = entity2.getContent();
+						BufferedReader reader = new BufferedReader(new InputStreamReader(instream));) {
+					String s;
+					while ((s = reader.readLine()) != null) {
+						System.out.println(s);
+						sb2.append(s);
+					}
+				}
+			}
+		}
+
+		
+		
+		
+		
+		
+		/*
+		 * HttpPost httppost = new
+		 * HttpPost("https://github.com/login/oauth/access_token"); List<NameValuePair>
+		 * params = new ArrayList<>(); params.add(new BasicNameValuePair("grant_type",
+		 * "authorization_code")); params.add(new BasicNameValuePair("code", code));
+		 * params.add(new BasicNameValuePair("redirect_uri",
+		 * Client.getRedirect_uris()[0]));
+		 * 
+		 * httppost.setEntity(new UrlEncodedFormEntity(params));
+		 * httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		 * httppost.setHeader("Authorization", "Basic " + Client.getClient_id() + ":" +
+		 * Base64.getEncoder().encodeToString(Client.getClient_secret().getBytes()));
+		 * 
+		 * TokenResponce tokenResponce = null; StringBuilder sb = new StringBuilder();
+		 * try( CloseableHttpResponse response = (CloseableHttpResponse)
+		 * httpclient.execute(httppost) ) { HttpEntity entity = response.getEntity(); if
+		 * (entity != null) { try ( InputStream instream = entity.getContent();
+		 * BufferedReader reader = new BufferedReader( new InputStreamReader(instream)
+		 * );){ String s; while((s = reader.readLine())!= null) { System.out.println(s);
+		 * sb.append(s); } } } }
+		 * 
+		 * tokenResponce = new Gson().fromJson( sb.toString() , TokenResponce.class);
+		 * 
+		 * token.setAccess_token(tokenResponce.getAccess_token());
+		 * token.setScope(tokenResponce.getScope());
+		 * token.setToken_type(tokenResponce.getToken_type());
+		 */
+
 		WebContext ctx = new WebContext(req, resp, getServletConfig().getServletContext(), req.getLocale());
 		TemplateEngine templateEngine = initTemplateEngine(this.getServletContext());
-		
-		req.setAttribute("access_token",token.getAccess_token());
-		req.setAttribute("scope",Arrays.toString(token.getScope()));
-		req.setAttribute("refresh_token","");
-		System.out.println(token);
+
+		// req.setAttribute("access_token",token.getAccess_token());
+		// req.setAttribute("scope",Arrays.toString(token.getScope()));
+		// req.setAttribute("refresh_token","");
+		// System.out.println(token);
 
 		templateEngine.process("index", ctx, resp.getWriter());
 	}
